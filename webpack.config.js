30c440dbc65081cf4c5b5f50fb3cb0a3e0bd4fee
yusuke higasa <higasa@studio-higasa.com>
2020-05-19 14:42:06 +0900

    Change public to docs

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = [
             ]
         },
         output: {
-            path: path.resolve(__dirname, "public"),
+            path: path.resolve(__dirname, "docs"),
             filename: "./assets/js/bundle.js"
         },
         module: {
